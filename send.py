a49481d369280cc818e12623e3b2dca9e6114d8b
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-04-13 01:53:08 -0400

    working as of last test

--- send.py
+++ send.py
@@ -1,12 +1,18 @@
-from twilio.rest import Client 
-from os import getenv as env
-from dotenv import load_dotenv
-from words import *
-load_dotenv()
+# from twilio.rest import Client 
+# from os import getenv as env
+# from dotenv import load_dotenv
+# from words import *
+# load_dotenv()
  
-account_sid = env("ACCOUNT_SID")
-auth_token = env("AUTH_TOKEN")
-client = Client(account_sid, auth_token) 
+# account_sid = env("ACCOUNT_SID")
+# auth_token = env("AUTH_TOKEN")
+# client = Client(account_sid, auth_token) 
 
 def send(word):
-    message = client.messages.create(messaging_service_sid=env("SERVICE_SID"), body=f"FYI, today's wordle is... {word}", from_=env("OUTGOING_PHONE_NUMBER"), to=env("RECIPIENT_PHONE_NUMBER")) 
+    pass
+
+# def send(word):
+#     message = client.messages.create(body=f"FYI, today's wordle is... {word}", from_=env("OUTGOING_PHONE_NUMBER"), to=env("RECIPIENT_PHONE_NUMBER")) 
+
+# if __name__ == "__main__":
+#     send("test")
